a99e582d42405fa2155ff42a3a7ba60d4308720e
Loic Baron <loic.baron@gmail.com>
2022-10-21 22:33:48 +0100

    doc: drought

---
+++ S_EXP_DRO.R
@@ -0,0 +1,12 @@
+# https://edo.jrc.ec.europa.eu/gdo/php/index.php?id=2112
+# SPI: https://edo.jrc.ec.europa.eu/documents/factsheets/factsheet_spi.pdf
+# https://edo.jrc.ec.europa.eu/gdo/php/index.php?id=2001
+
+# https://energydata.info/dataset/global-drought-hazard-0
+
+# https://resourcewatch.org/data/explore/cli023-Standard-Precipitation-Index
+# https://resourcewatch.org/data/explore/wat057-Aqueduct-Drought-Risk
+# https://resourcewatch.org/data/explore/foo_058_SMAP_soil_moisture
+
+# https://www.sciencedirect.com/science/article/pii/S0959378016300565#bib0420
+# 
