b7eca5b97e59ee823d1ff09da9bd607032967269
Loic Baron <loic.baron@gmail.com>
2022-10-21 22:33:32 +0100

    doc: storm surge - coastal flood

---
+++ S_EXP_COF.R
@@ -0,0 +1,5 @@
+# https://energydata.info/dataset/global-coastal-flood-hazard
+# https://www.frontiersin.org/articles/10.3389/fmars.2020.00263/full
+
+# data: https://zenodo.org/record/3660927#.Y1JwOuzMKDU
+# Mesh Layer QGIS: https://gis.stackexchange.com/questions/357159/cannot-open-netcdf-file-in-qgis
