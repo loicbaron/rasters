2557b9d1cc08c6cf4a625b031c2a3b5a93f51a81
Loic Baron <loic.baron@gmail.com>
2022-10-21 22:33:56 +0100

    doc: salinity

---
+++ S_EXP_SAL.R
@@ -0,0 +1,9 @@
+# Bangladesh
+# https://www.nature.com/articles/s41597-020-0562-z#Sec7
+# https://doi.pangaea.de/10.1594/PANGAEA.913939?format=html#download
+
+# Vietnam - Mekong
+# https://data.opendevelopmentmekong.net/dataset/saline-intrusion-in-the-southern-region-vietnam
+
+# salinity electrical conductivity
+# https://www.fondriest.com/environmental-measurements/parameters/water-quality/conductivity-salinity-tds/
