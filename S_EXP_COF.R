466d525536d2aef587778ea267c9821600f1dcd5
Loic Baron <loic.baron@gmail.com>
2022-10-31 00:11:35 +0000

    add coastal floods storm surge

--- S_EXP_COF.R
+++ S_EXP_COF.R
@@ -1,5 +1,29 @@
+library(raster)
+library(sf)
+
+source("helpers.R")
+
+
+locations <- readRDS("data/objects/locations")
 # https://energydata.info/dataset/global-coastal-flood-hazard
 # https://www.frontiersin.org/articles/10.3389/fmars.2020.00263/full
 
 # data: https://zenodo.org/record/3660927#.Y1JwOuzMKDU
 # Mesh Layer QGIS: https://gis.stackexchange.com/questions/357159/cannot-open-netcdf-file-in-qgis
+my_raster <- raster("data/StormSurge/ss_muis_rp0100m.tif")
+
+r2 <- crop(my_raster, extent(locations)) ### crop to the extent
+my_raster_clip <- mask(x = r2, mask = locations) ### delimitation to the shape geometry
+plot(my_raster_clip)
+
+### zonal statistics using "raster"
+locations$cnt <- extract(my_raster_clip, locations, fun = mean)
+
+# library(terra)
+# r <- rast(ras)
+# v <- vect(sp)
+# extract(r, v, "mean")
+
+locations$freq <- locations$cnt / locations$pop
+locations$norm <- normalize_minmax(locations$freq, na.rm = TRUE)
+st_write(locations, "output/S_EXP_COF_floods.shp", layer_options = "ENCODING=UTF-8", append = FALSE)
