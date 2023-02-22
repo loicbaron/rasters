c67c8b8bed0ab62f4008c4505854559996de88b1
Loic Baron <loic.baron@gmail.com>
2023-02-22 21:24:49 +0000

    update S_EXP_FLO

--- S_EXP_FLO.R
+++ S_EXP_FLO.R
@@ -1,27 +1,26 @@
+library(cleaner)
 library(raster)
 library(sf)
 
 source("helpers.R")
 
-
 locations <- readRDS("data/objects/locations")
 # data source: UNEP
-# # https://wesr.unepgrid.ch/?project=MX-XVK-HPH-OGN-HVE-GGN&language=en
 # https://wesr.unepgrid.ch/static.html?views=MX-JXZXA-MFZNN-LTXZ8&zoomToViews=true
-my_raster <- raster("data/Floods/fl_hazard_100_yrp.tif")
+floods <- raster("data/Floods/fl_hazard_100_yrp.tif")
 
-r2 <- crop(my_raster, extent(locations)) ### crop to the extent
-my_raster_clip <- mask(x = r2, mask = locations) ### delimitation to the shape geometry
-plot(my_raster_clip)
+floods_extent <- crop(floods, extent(locations)) ### crop to the extent
+floods_roi <- mask(x = floods_extent, mask = locations) ### delimitation to the shape geometry
+plot(floods_roi)
 
 ### zonal statistics using "raster"
-locations$cnt <- extract(my_raster_clip, locations, fun = mean)
+locations$cnt <- extract(floods_roi, locations, fun = mean, na.rm=TRUE) %>% na_replace()
 
 # library(terra)
 # r <- rast(ras)
 # v <- vect(sp)
 # extract(r, v, "mean")
 
-locations$freq <- locations$cnt / locations$pop
+locations$freq <- locations$cnt * locations$pop
 locations$norm <- normalize_minmax(locations$freq, na.rm = TRUE)
 st_write(locations, "output/S_EXP_FLO_floods.shp", layer_options = "ENCODING=UTF-8", append = FALSE)
